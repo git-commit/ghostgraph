94fe16c9d5d6204c3f218b3a7f2ac0e3e64901f1
Simon Goller <neosam+github@posteo.de>
2023-10-31 21:25:59 +0100

    Awesome graph!

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+pub struct Node<'a> {
+    incoming: Vec<&'a ghost_cell::GhostCell<'a, Node<'a>>>,
+    outgoing: Vec<&'a ghost_cell::GhostCell<'a, Node<'a>>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<'a> Node<'a> {
+    pub fn new() -> Self {
+        Self {
+            incoming: Vec::new(),
+            outgoing: Vec::new(),
+        }
     }
+}
+
+pub fn add_edge<'a>(
+    node1: &'a ghost_cell::GhostCell<'a, Node<'a>>, 
+    node2: &'a ghost_cell::GhostCell<'a, Node<'a>>, 
+    token: &mut ghost_cell::GhostToken<'a>) 
+{
+    node1.borrow_mut(token).outgoing.push(node2);
+    node2.borrow_mut(token).incoming.push(node1);
 }
